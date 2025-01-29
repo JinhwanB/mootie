574624df317fe3f800171a44bed1f7b22d1e20a4
jinhwanB <paewhal20@gmail.com>
2025-01-29 17:30:22 +0900

    init

---
+++ src/main/java/com/jinhwanb/mootie/MootieApplication.java
@@ -0,0 +1,13 @@
+package com.jinhwanb.mootie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MootieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MootieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jinhwanb/mootie/MootieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinhwanb.mootie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MootieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
