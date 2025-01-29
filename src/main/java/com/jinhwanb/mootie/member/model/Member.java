0fa38d12ec6f629e5a3122490f2ff4be014c058b
jinhwanB <paewhal20@gmail.com>
2025-01-29 21:38:47 +0900

    Update: 회원 엔티티에 삭제 날짜 필드 추가
    소프트 딜리트를 위한 삭제 날짜 필드 추가

--- src/main/java/com/jinhwanb/mootie/member/model/Member.java
+++ src/main/java/com/jinhwanb/mootie/member/model/Member.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -40,4 +41,7 @@ public class Member extends BaseTImeEntity {
 
     @Column(nullable = false)
     private String email; // 회원 이메일
+
+    @Column
+    private LocalDateTime deletedAt; // 삭제 날짜
 }
